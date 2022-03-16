254df8653b94f04c9fb65e6fb4800dd02895a9f4
Enrico Schifano <enrs@bendingspoons.com>
2022-03-16 10:53:40 +0100

    add scripts & theme

---
+++ update_keybindings.sh
@@ -0,0 +1,2 @@
+rm Eraz1997.idekeybindings
+cp ~/Library/Developer/Xcode/UserData/KeyBindings/Eraz1997.idekeybindings .
---
+++ update_theme.sh
@@ -0,0 +1,2 @@
+rm Eraz1997DarkTheme.xccolortheme
+cp ~/Library/Developer/Xcode/UserData/FontAndColorThemes/Eraz1997DarkTheme.xccolortheme .
